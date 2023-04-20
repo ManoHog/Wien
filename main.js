0caf2f80888a4ad41eae132bc9dee499672d0dc9
Your Name <you@example.com>
2023-04-20 18:10:02 +0200

    Sehenswürdigkeiten vorbereitet

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ let map = L.map("map").setView([
 ], 12);
 // thematische Layer
 let themaLayer = {
-    stops: L.featureGroup().addTo(map),
-    lines: L.featureGroup().addTo(map),
-    zones: L.featureGroup().addTo(map),
+    stops: L.featureGroup(),
+    lines: L.featureGroup(),
+    zones: L.featureGroup(),
     sites: L.featureGroup().addTo(map)
 }
 
@@ -46,7 +46,7 @@ async function showStops(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
     L.geoJSON(jsondata).addTo(themaLayer.stops);
-    console.log(response, jsondata)
+    //console.log(response, jsondata)
 }
 showStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
 
@@ -55,7 +55,7 @@ async function showLines(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
     L.geoJSON(jsondata).addTo(themaLayer.lines);
-    console.log(response, jsondata)
+    //console.log(response, jsondata)
 }
 showLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json")
 
@@ -64,7 +64,7 @@ async function showZones(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
     L.geoJSON(jsondata).addTo(themaLayer.zones);
-    console.log(response, jsondata)
+    //console.log(response, jsondata)
 }
 showZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json")
 
